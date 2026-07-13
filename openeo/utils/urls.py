38e6e013fa593d04b7d6a649f1d51c47ad614c80
samdoolin <sam.doolin@assimila.eu>
2026-07-13 16:36:20 +0100

    S1 run-udp

--- openeo/utils/urls.py
+++ openeo/utils/urls.py
@@ -3,3 +3,6 @@ NATURAL_FOREST_STAC = "https://s3.waw3-2.cloudferro.com/swift/v1/leon-p6/natural
 
 # TODO: point to main branch once merged
 FOREST_BASELINE_UDP = "https://raw.githubusercontent.com/Assimila/LEON-P6/refs/heads/openeo/openeo/forest-baseline/udp.json"
+
+# TODO: point to main branch once merged
+S1_PROCESSING_UDP = "https://raw.githubusercontent.com/Assimila/LEON-P6/refs/heads/openeo/openeo/s1-processing/udp.json"
